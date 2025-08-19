dd0942507d371dbcc1ee0c35fe057effd144b8a3
Muhammad Sulais <muh.sulais@gmail.com>
2025-08-20 00:09:41 +0700

    fix duplicate list

--- code.ts
+++ code.ts
@@ -5,7 +5,7 @@ type FigmaMessage = {
     generatorType: GeneratorType
 }
 
-const ALL_LOREM_IPSUM = "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".split(' ')
+const ALL_LOREM_IPSUM = ['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'aliquip', 'ex', 'ea', 'commodo', 'consequat', 'duis', 'aute', 'irure', 'in', 'reprehenderit', 'voluptate', 'velit', 'esse', 'cillum', 'eu', 'fugiat', 'nulla', 'pariatur', 'excepteur', 'sint', 'occaecat', 'cupidatat', 'non', 'proident', 'sunt', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum']
 const MIN_WORDS_PER_SENTENCE = 7
 const MAX_WORDS_PER_SENTENCE = 20
 const MIN_SENTENCE_PER_PARAGRAPH = 3
